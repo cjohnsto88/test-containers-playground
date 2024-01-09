9c63c3b13cf96fc8aa5a628a60746225b9209bd6
Craig Johnston <cjohnsto88@gmail.com>
2024-01-09 20:59:32 +0000

    Added test that shows Wiremock running in a container

--- src/test/java/com/example/testcontainers/TestContainerPlaygroundApplicationTests.java
+++ src/test/java/com/example/testcontainers/TestContainerPlaygroundApplicationTests.java
@@ -5,16 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +28,10 @@ class TestContainerPlaygroundApplicationTests {
     @Container
     @ServiceConnection
     static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
+
+    @Container
+    static WireMockContainer wireMockContainer = new WireMockContainer("wiremock/wiremock:2.35.0")
+            .withMappingFromResource("example", "wiremock/stub.json");
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -49,7 +56,17 @@ class TestContainerPlaygroundApplicationTests {
         assertThat(rowCount).isEqualTo(1);
     }
 
-	private void postEmployee(String requestJson) {
+    @Test
+    void wiremockStubReturnsExpectedResponse() {
+        RestTemplate restTemplate = new RestTemplateBuilder().rootUri("http://localhost:" + wireMockContainer.getPort())
+                .build();
+
+        String responseBody = restTemplate.getForObject("/some/thing", String.class);
+
+        assertThat(responseBody).isEqualTo("Hello, world!");
+    }
+
+    private void postEmployee(String requestJson) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
